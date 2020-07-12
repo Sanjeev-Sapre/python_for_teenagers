ce0a6dc7cd8ad7875ad3748e0c781623d036b26e
Sanjeev-Sapre <sanjeev.sapre@gmail.com>
2020-07-12 23:54:20 +0530

    Adding Day-04

---
+++ Day-04/grade_calculation2.py
@@ -0,0 +1,13 @@
+percent = int( input("enter your perent :"))
+
+
+if percent > 75 :
+    print("Distinction")
+elif percent > 60:
+    print('First Class')
+elif percent > 40:
+    print("Second Class")
+elif percent < 40 :
+    print( "Failed")
+
+
